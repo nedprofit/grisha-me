abab0c976c48738db56ce31f1597577b1e75cb2b
nouara <nedprofit@gmail.com>
2020-09-25 15:27:33 +0300

    init

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "grisha", password: "rails", except: [:index, :show]
-
   def index
     @articles = Article.all
   end
