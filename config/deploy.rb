4e75c7288899ba34500420366c467d11e43695c9
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2022-08-29 12:06:57 +0300

    1

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,21 +18,6 @@ namespace :bundler do
   desc 'bundle config options'
 end
 
-append :linked_files, "config/master.key"
-set :linked_files, %w{config/master.key}
-
-namespace :deploy do
-  namespace :check do
-    before :linked_files, :set_master_key do
-      on roles(:app), in: :sequence, wait: 10 do
-        unless test("[ -f #{shared_path}/config/master.key ]")
-          upload! 'config/master.key', "#{shared_path}/config/master.key"
-        end
-      end
-    end
-  end
-end
-
 set :passenger_restart_with_touch, true
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
