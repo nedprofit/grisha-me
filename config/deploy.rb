256cfecf101ccc6d478d60866079b30f03c7ddba
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2022-08-29 11:15:41 +0300

    1

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,12 +13,6 @@ append :linked_files, "config/master.key"
 namespace :bundler do
   before 'bundler:install', :config
   desc 'bundle config options'
-  task :config do
-    on roles(:all), in: :groups, limit: 3, wait: 10 do
-      # Required for pg gem to be installed
-      execute 'bundle config build.pg --with-pg-config=/usr/pgsql-13/bin/pg_config'
-    end
-  end
 end
 set :passenger_restart_with_touch, true
 
